4dcfb9d3efe3cdecd115689b10724258b10515ed
Aleksandr Ruban <ruban_1998@hotmail.com>
2023-05-01 21:45:47 +0500

    feat: add docstring

--- strite_data_hub/dataclasses/prediction.py
+++ strite_data_hub/dataclasses/prediction.py
@@ -1,17 +1,17 @@
-from datetime import datetime
-from decimal import Decimal
 from dataclasses import dataclass
-from typing import Self, Optional
 
 
 @dataclass()
 class PredictionFOS:
     """Фиксированный размер заказа
+
+    Attributes:
+        safety_stock:        Страховой запас
+        max_reserve:         Максимальный запас
+        order_point:         Точка заказа
+        average_stock_level: Средний уровень запаса
     """
     safety_stock: float = 0
     max_reserve: float = 0
     order_point: float = 0
     average_stock_level: float = 0
-
-
-
