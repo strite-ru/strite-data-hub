f79ba2594dcfb38c92f36111e985b3440d8c6412
Aleksandr Ruban <ruban_1998@hotmail.com>
2023-09-04 22:17:54 +0500

    change: up version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     py_modules=["strite_data_hub"],
     packages=find_packages(exclude=['tests', 'tests.*']),
     install_requires=[
-        "requests==2.28.2",
+        "requests==2.31.0",
         "catboost==1.2.1",
         "urllib3==2.0.4"
     ]
